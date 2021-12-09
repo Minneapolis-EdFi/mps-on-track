adfbd465e7da3e2849cd0c08eeb8958ef2e02f60
hokafor <h.okafor@wearedoubleline.com>
2021-12-09 10:10:30 -0600

    Add Comment Text

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -79,7 +79,8 @@ const App: FC = () => {
 			{!isProd && !isIE && (
 				<footer className="bg-gray-800 mt-4 px-2">
 					<code className="text-white mr-4">
-						Env: {process.env.APP_ENV} ({process.env.COMMIT_HASH})
+						{/* Env: {process.env.APP_ENV} ({process.env.COMMIT_HASH}) */}
+						You must complete your My Life Plan requirement. If you have not completed this, please speak with your counselor.
 					</code>
 				</footer>
 			)}
